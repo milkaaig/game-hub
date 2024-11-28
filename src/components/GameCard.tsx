70009fe1dafaa1da6ce4f67d8a6739d5a0733592
milkaaig <milkaaigetachew@gmail.com>
2024-11-28 21:48:25 +0300

    Add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,29 +1,32 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames"
+import { Game } from "../hooks/useGames";
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
 import getCropperImageUrl from "../services/imageUrl";
+import Emoji from "./Emoji";
 
-interface Props{
-    game: Game;
+interface Props {
+  game: Game;
 }
 
-
-const GameCard = ({game}: Props) => {
+const GameCard = ({ game }: Props) => {
   return (
-   <Card >
-    <Image src={getCropperImageUrl(game.background_image)}/>
-    <CardBody>
-        <HStack justifyContent='space-between' marginBottom={3}>
-            <PlatformIconsList platforms={game.parent_platforms.map(p => p.platform)}/>
-            <CriticScore score={game.metacritic}/>
+    <Card>
+      <Image src={getCropperImageUrl(game.background_image)} />
+      <CardBody>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconsList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize={"2xl"}>
-            {game.name}
+          {game.name}
+          <Emoji rating={game.rating_top} />
         </Heading>
-    </CardBody>
-   </Card>
-  )
-}
+      </CardBody>
+    </Card>
+  );
+};
 
-export default GameCard
+export default GameCard;
