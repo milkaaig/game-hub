9a00aa0ff4f165fb73286bb5ac9a080d15c90910
milkaaig <milkaaigetachew@gmail.com>
2024-11-28 19:23:48 +0300

    Clean up the game cards

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const heading = `${gameQuery.platform?.name || ''} ${gameQuery?.genre || ''} Games`;
+  const heading = `${gameQuery.platform?.name || ''} ${gameQuery?.genre?.name || ''} Games`;
   return <Heading as="h1" fontSize='5xl'  marginX={2} >{heading}</Heading>;
 };
 
